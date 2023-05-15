2f3fe60eb2ee2c682974a711096ac3e8affb7426
Saidwaraki17 <75656407+Saidwaraki17@users.noreply.github.com>
2023-05-15 08:18:26 +0000

    added a job page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask ,render_template, jsonify
+from flask import Flask, render_template, jsonify
 
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 from sqlalchemy import text
 
@@ -9,7 +9,7 @@ from sqlalchemy import text
 
 # so we create a object in python named app
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
 # __name__ returns __main__ function
 '''
@@ -49,23 +49,33 @@ JOBS = [
 
 # next we add a @oject.route(host = 'address', debug = 'True') method to object where the url triggered to other page
 
-  
-@app.route("/") # this is empty page 
+
+@app.route("/")  # this is empty page
 def hello_world():
   list_of_jobs = load_jobs_from_db()
-  return render_template('home.html', 
-                         jobs = list_of_jobs,
-                        company_name = "EBAIEE FUTRUZ")
+  return render_template('home.html',
+                         jobs=list_of_jobs,
+                         company_name="EBAIEE FUTRUZ")
+
 
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
-if __name__ == '__main__':
-  app.run(host = '0.0.0.0', debug=True) # we need to give a host address 
-# if we add 0.0.0.0 it runs in local environent 
 
-# Flask and gunicorn-- a development based server which is not used for production ,are added into requirements.txt which is standard file for build command at render. 
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found!", 404
+  return render_template('jobpage.html', job=job)
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)  # we need to give a host address
+# if we add 0.0.0.0 it runs in local environent
+
+# Flask and gunicorn-- a development based server which is not used for production ,are added into requirements.txt which is standard file for build command at render.
 
 # To put a Flask application into production we need gunicorn library.
--- database.py
+++ database.py
@@ -1,21 +1,32 @@
 from sqlalchemy import create_engine, text
-
+import os
 import pymysql
 
-db_connection_str = "mysql+pymysql://a1qg70uvy4ubgeaqbjkh:pscale_pw_g25Ft6IF48KgqubxjbW86C5CfwAmUVsywZnHsLLGZvm@aws.connect.psdb.cloud/careerswebsite?charset=utf8mb4"
+db_connection_str = "mysql+pymysql://j8du6hiitogvd40eaaz3:pscale_pw_CKtIg2NZC2Vrf3wWuhbmodjMsGDd5CvcTUklXkVDvBy@aws.connect.psdb.cloud/career-website?charset=utf8mb4"
 engine = create_engine(db_connection_str,
-                      connect_args={
-                        "ssl" :{
-                        "ssl_ca":"/etc/ssl/cert.pem"
-                        }
-                      })
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
 
 def load_jobs_from_db():
   with engine.connect() as conn:
-   result = conn.execute(text("select * from jobs"))
-   result_dict = []
-   for row in result.all():
-     result_dict.append(row._asdict())
-   return result_dict
+    result = conn.execute(text("select * from jobs"))
+    result_dict = []
+    for row in result.all():
+      result_dict.append(row._asdict())
+    return result_dict
 
-  
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text(f"SELECT * FROM jobs WHERE id = :val"),
+      {"val": id}
+    )
+    rows = result.mappings().all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
+    
+    
